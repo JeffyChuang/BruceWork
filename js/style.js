901beb3183c2d615e940b573d512939ae29cbd40
bruceLinEX <dvd2631@gmail.com>
2016-09-12 22:25:15 +0800

    0912

---
+++ js/style.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.poster1-js',{ duration:1000,origin: 'bottom',scale:0.5});
+sr.reveal('.poster1-js-text',{ duration:1000,origin: 'left',delay:500,scale:0.5});
+

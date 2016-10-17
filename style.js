5f1a390bcd9e669ee97ac1f340e903f0c89f4efa
bruceLinEX <dvd2631@gmail.com>
2016-10-18 07:53:54 +0800

    resume

--- style.js
+++ style.js
@@ -6,16 +6,24 @@ sr.reveal('.poster1-js-text2',{ duration:1000,origin: 'top',delay:1000,scale:0.5
 
 	$('.hamber').click(function(){
 		if($('.navbar_1').css("left") == "-250px"){
+			$('.i-span1').css({display:'none'});
+			$('.i-span2').css({display:'none'});
+			$('.i-span3').css({display:'none'});
+			$('.i-span4').css({display:'block',transform:'scale(1,1)',transition:'3s all'});
 			$('.navbar_1').animate({
 				left:'0px'
-			},1000,'easeInBounce');
+			},1000,'easeOutCirc');
 			$('.section').animate({
 				left:'250px'
-			},1000,'easeInBounce');
+			},1000,'easeOutCirc');
 			$('.hamber').animate({
 				left:'270px'
-			},1000,'easeInBounce');
+			},1000,'easeOutCirc');
 		}else if($('.navbar_1').css("left") == "0px"){
+			$('.i-span1').css({display:'block'});
+			$('.i-span2').css({display:'block',});
+			$('.i-span3').css({display:'block',});
+			$('.i-span4').css({display:'none'});
 			$('.navbar_1').animate({
 				left:'-250px'
 			},1000,'easeOutBounce');
@@ -28,15 +36,65 @@ sr.reveal('.poster1-js-text2',{ duration:1000,origin: 'top',delay:1000,scale:0.5
 
 		}
 	});
+$('.HOME').click(function(){
+$('.HOME').css({color:'#ffa500'});
+$('.ABOUT').css({color:'#fff'});
+$('.RESUME').css({color:'#fff'});
+$('.SERVICE').css({color:'#fff'});
+$('.CONTACT').css({color:'#fff'});
 
+});
 
+$('.ABOUT').click(function(){
+$('.HOME').css({color:'#fff'});
+$('.ABOUT').css({color:'#ffa500'});
+$('.RESUME').css({color:'#fff'});
+$('.SERVICE').css({color:'#fff'});
+$('.CONTACT').css({color:'#fff'});
 
+});
+$('.RESUME').click(function(){
+$('.HOME').css({color:'#fff'});
+$('.ABOUT').css({color:'#fff'});
+$('.RESUME').css({color:'#ffa500'});
+$('.SERVICE').css({color:'#fff'});
+$('.CONTACT').css({color:'#fff'});
 
+});
+$('.SERVICE').click(function(){
+$('.HOME').css({color:'#fff'});
+$('.ABOUT').css({color:'#fff'});
+$('.RESUME').css({color:'#fff'});
+$('.SERVICE').css({color:'#ffa500'});
+$('.CONTACT').css({color:'#fff'});
+
+});
+$('.CONTACT').click(function(){
+$('.HOME').css({color:'#fff'});
+$('.ABOUT').css({color:'#fff'});
+$('.RESUME').css({color:'#fff'});
+$('.SERVICE').css({color:'#fff'});
+$('.CONTACT').css({color:'#ffa500'});
+
+});
 
 
 
 });
 
+
+function doFirst() {
+var Ham = document.getElementsByClassName('i-span1');
+Ham.css="display:none";
+}
+function hamberclick(){
+
+
+}
+
+
+
+window.addEventListener('load', doFirst, false);
 
 
 
